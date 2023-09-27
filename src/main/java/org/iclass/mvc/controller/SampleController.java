dbf955af35b6b48cf29f254773b73f174fdd7010
MOMOESOON <sbag17230@gmail.com>
2023-09-27 14:05:55 +0900

    1일차

---
+++ src/main/java/org/iclass/mvc/controller/SampleController.java
@@ -0,0 +1,34 @@
+package org.iclass.mvc.controller;
+
+import org.iclass.mvc.service.CommunityService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.thymeleaf.model.IModel;
+
+import java.util.List;
+
+//  url 이 community 로 시작하면 DispatcherServlet 으로부터 CommunityController가 위임받아 처리합니다.
+@Controller
+@RequestMapping("/sample")	
+public class SampleController {
+
+	private final CommunityService service;
+	
+	private SampleController(CommunityService service) {
+		this.service=service;
+	}
+	
+	//여기서부터는 요청을 처리하는 핸들러 메소드입니다.
+	//URL 설계 :  글 목록 조회는 /community/list , 글쓰기 /community/write , 글읽기 /community/read  
+
+	
+	@GetMapping("/hello")
+	public void hello(Model model) {
+		model.addAttribute("message","하이 스프링");
+		model.addAttribute("list",List.of("모모","나연","nana","쯔위"));
+	}
+	
+
+}
---
+++ src/main/resources/templates/list.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="EUC-KR">
+<title>커뮤니티 리스트</title>
+</head>
+<body>
+<a href="list">글목록</a> 
+</body>
+</html>
